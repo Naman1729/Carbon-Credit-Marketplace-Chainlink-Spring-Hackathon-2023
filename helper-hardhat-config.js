b0ae336455fadbffe9a3e65bd9feebe01ebf5300
shikhar229 <shikharagarwale7@gmail.com>
2023-06-04 21:56:39 +0530

    modified emission and weather feeds

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,8 +1,8 @@
 const networkConfig = {
     80001: {
         name: "mumbai",
-        emissionFeeds: "0x5643a974BaE022Fb61fB24d837f33cBe08372fb8",
-        weatherFeeds: "0x3EF9800eE7781C655Dd694FE44700200f45E1a6d",
+        emissionFeeds: "0x12D48b7Fa83dCF26b91591A63349048173c6a288",
+        weatherFeeds: "0x60e55982f7DF115D58eae49E631eE2FdC8aB0Ef6",
         admins: ["0xc15BB2baF07342aad4d311D0bBF2cEaf78ff2930", "0xF1c8170181364DeD1C56c4361DED2eB47f2eef1b"],
         approvalsRequired: 2,
         reviewInterval: 0
